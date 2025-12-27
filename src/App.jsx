77b3da84af1b40fa65090d01471fa287129f1b51
skywalker <skywalkerwang98@gmail.com>
2025-12-27 13:20:16 +0800

    debug: Temporarily disable DragDropProvider to test window dragging

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import { useState } from 'react'
 import Desktop from './components/layout/Desktop'
 import BootSequence from './components/system/BootSequence'
 import { useStore } from './store/useStore'
-import { DragDropProvider } from './context/DragDropContext'
+// Temporarily disabled to debug window dragging
+// import { DragDropProvider } from './context/DragDropContext'
 
 function App() {
     const { isLogin } = useStore()
 
     return (
-        <DragDropProvider>
-            <div className="w-screen h-screen overflow-hidden">
-                {isLogin ? <Desktop /> : <BootSequence />}
-            </div>
-        </DragDropProvider>
+        <div className="w-screen h-screen overflow-hidden">
+            {isLogin ? <Desktop /> : <BootSequence />}
+        </div>
     )
 }
 
